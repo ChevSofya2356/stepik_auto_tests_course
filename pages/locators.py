d10566ee29d3abf5b27d1ad9f5c1dc02ed2bb2f9
ChevSofya2356 <cevakinasona@gmail.com>
2026-03-13 01:52:08 +0300

    Delete pages/locators.py

--- pages/locators.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class ProductPageLocators:
-    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
-    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
-    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
-    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success .alertinner strong")
-    BASKET_TOTAL_MESSAGE = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
